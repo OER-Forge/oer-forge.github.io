f635c877518744a91b54aacbc6f80f7fda2ddf1b
Danny Caballero <danny.caballero@pm.me>
2025-07-10 21:17:40 -0400

    starting scan.py

---
+++ oerforge/scan.py
@@ -0,0 +1,26 @@
+"""
+Module: scan
+
+Purpose: Scans _config.yml and content/ for site information, table of contents information, and page information. Writes SQLite database with three tables: site, toc, and page.
+"""
+
+def read_site_info_from_config():
+    
+def write_site_info_to_db():
+    
+def read_toc_from_config():
+    
+def write_toc_to_db():
+    
+def read_toc_item_from_db():
+    
+def write_toc_item_to_db():
+    
+def read_page_info_from_config():
+    
+def write_page_info_to_db():
+    
+def read_page_item_from_db():
+
+def write_page_item_to_db():
+
